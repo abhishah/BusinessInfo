793e64ebf7e428e8892d014647510c96bfe04cc0
ankurgarg101 <ankurgarg101@gmail.com>
2015-03-18 18:55:25 +0530

    Add buttons for call and email

---
+++ BusinessInfo/src/in/co/info/business/Payments.java
@@ -0,0 +1,14 @@
+package in.co.info.business;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Payments extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
